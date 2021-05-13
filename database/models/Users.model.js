d9564ba02ce7d7a25d8a0a17d3b247a247e1fb1d
YuriiSynyavskiy <53538660+YuriiSynyavskiy@users.noreply.github.com>
2021-05-14 00:10:03 +0300

    upd user model

--- database/models/Users.model.js
+++ database/models/Users.model.js
@@ -1,82 +1,82 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'Users',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: false,
-      },
-      surname: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: false,
-      },
-      pass: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: false,
-      },
-      mail: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      refreshToken: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      photoUrl: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      role: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      favouritesHousesIds: {
-        type: DataTypes.ARRAY(DataTypes.INTEGER),
-        unique: false,
-        allowNull: true,
-      },
-      country: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      city: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      region: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-      instProfile: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: true,
-      },
-      bio: {
-        type: DataTypes.STRING,
-        unique: false,
-        allowNull: true,
-      },
-    },
-    {
-      tableName: 'Users',
-      timestamps: false,
-    }
-  )
-  return User
-}
+    const User = sequelize.define('Users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false
+        },
+        surname: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false
+        },
+        pass: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false
+        },
+        mail: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        refreshToken: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+        photoUrl: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true,
+        },
+        role: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true,
+        },
+        favouritesHousesIds: {
+            type: DataTypes.ARRAY(DataTypes.INTEGER),
+            unique: false,
+            allowNull: true
+        },
+        country: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+        city: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+        region: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+        instProfile: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: true
+        },
+        bio: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+        mobilePhone: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        },
+    }, {
+        tableName: 'Users',
+        timestamps: false
+    });
+    return User;
--- router/usersRouter/index.js
+++ router/usersRouter/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 
 const middleware = require('../../middleware')
 const {
-  showByID,
-  change,
-  remove,
-  showHouses,
-  addFavourite,
-  changeProfilePhoto,
+    showByID,
+    change,
+    remove,
+    showHouses,
+    addFavourite,
+    changeProfilePhoto,
 } = require('../../controllers/usersController')
 
 router.patch('/changeProfilePhoto', middleware.checkFiles, changeProfilePhoto)
@@ -17,5 +17,14 @@ router.get('/my-profile', middleware.checkAccess, showByID)
 router.patch('/:id', change)
 router.delete('/:id', remove)
 router.post('/addFavourite', addFavourite)
+
+router.patch('/changeProfilePhoto', middleware.checkFiles, changeProfilePhoto);
+//router.get('/', showAll);
+router.get('/userHouses', showHouses);
+router.get('/my-profile', showByID);
+router.patch('/my-profile', change);
+router.delete('/:id', remove);
+router.post('/addFavourite', addFavourite);
+
 
 module.exports = router
